686ac4dc1ad52248e9d5054d80e06332d69b5574
poojapsuku <poojapsuku@gmail.com>
2022-06-15 19:06:23 +0530

    MenuDriven Commit

---
+++ Menudriven.py
@@ -0,0 +1,13 @@
+menu=["1.Chapathi","2.Noodles","3.Biriyani","Porotta","Fried Rice "]
+print(menu)
+choice=int(input("Enter Your Choice:"))
+if choice==1:
+    print("Your Choice is CHAPATHI")
+elif choice==2:
+    print("Your Choice is NOODLES")
+elif choice==3:
+    print("Your Choice is BIRIYANI")
+elif choice==4:
+    print("Your Choice is POROTTA")
+else:
+    print("Your Choice is FRIED RICE")
