973c9531dc43054bf329351d7ef6620a57450425
poojapsuku <poojapsuku@gmail.com>
2022-06-15 13:39:27 +0530

    ifSample Commit

---
+++ if_sample.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a Number::"))
+if num < 0:
+    print("number is negative")
+elif num== 0:
+    print("Zero is Either Positive or Negative")
+else:
+    print("Number is Positive")
