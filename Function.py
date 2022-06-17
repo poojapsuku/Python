bb93053cd9314dd15173db0ad8c7ec22ba8ee6b6
poojapsuku <poojapsuku@gmail.com>
2022-06-17 22:02:13 +0530

    Function

---
+++ Function.py
@@ -0,0 +1,20 @@
+"""def add(a, b):
+    sum1 = a + b
+    return sum1
+
+
+a = int(input("Enter a Value::"))
+b = int(input("Enter b Value::"))
+print("Sum of the numbers = " + str(add(a, b)))"""
+
+
+# Arbitary Argument
+def hey(*values):
+
+    for x in value:
+
+        print (value[x])
+
+
+value = ["Pooja"]
+hey(value)
