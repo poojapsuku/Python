4260f7a7c535b4567c7519f1c428d8e4f8aa276b
poojapsuku <poojapsuku@gmail.com>
2022-06-26 14:03:32 +0530

    Function

--- Function.py
+++ Function.py
@@ -10,11 +10,43 @@ print("Sum of the numbers = " + str(add(a, b)))"""
 
 # Arbitary Argument
 def hey(*values):
-
-    for x in value:
-
-        print (value[x])
+    print(" da"+values[1])
 
 
-value = ["Pooja"]
-hey(value)
+hey("Pooja","Pooh")
+
+# Return Values
+def rtn(x):
+    return 10 * x
+
+print(rtn(20))
+print(rtn(10))
+
+# Passing a list as an argument
+
+def My_fun(cars):
+    for x in cars:
+        print(x)
+
+car=["Audi","BMW","Ford","Innova"]
+My_fun(car)
+
+
+# Keyword Argument
+
+def fun(name,age):
+    print(name,age)
+
+fun(age=10,name="Pooja")
+
+# dafault argument
+def fun(name,age=20):
+    print(name,age)
+
+fun(name="Pooja",age=23)
+
+# sum
+def sum(num1,num2):
+    sum1=num1+num2
+    return sum1
+print(sum(10,15))
